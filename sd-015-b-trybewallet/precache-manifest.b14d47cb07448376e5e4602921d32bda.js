c8ae66b971952573a491bcbe39213ee59d264009
Antonio Carlos <tonhaun@yahoo.com.br>
2021-11-29 11:26:35 -0300

    (Teste)Adiciona o projeto trybewallet

---
+++ sd-015-b-trybewallet/precache-manifest.b14d47cb07448376e5e4602921d32bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7fcac6423023abe68d3b167a01955c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a26e594dca9d3a3c971",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "f69e3f2926b02a270805",
+    "url": "/static/js/2.76bef402.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.76bef402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a26e594dca9d3a3c971",
+    "url": "/static/js/main.293d06e1.chunk.js"
+  },
+  {
+    "revision": "fe617c2cb41aece9e721",
+    "url": "/static/js/runtime-main.e8df815d.js"
+  }
+]);
